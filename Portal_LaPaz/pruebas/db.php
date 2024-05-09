23116239b47fb5b878bf22454a2044cdcca2445d
Lovoh17 <115592738+Lovoh17@users.noreply.github.com>
2024-05-08 22:01:48 -0600

    foteer

--- Portal_LaPaz/pruebas/db.php
+++ Portal_LaPaz/pruebas/db.php
@@ -1,42 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    $connection = pg_connect("host=localhost dbname=db_cLapaz user=postgres password=Ally.2203");
-    if (!$connection) {
-        echo "LA CAGASTE EN: " . pg_last_error();
-    } else {
-        echo "Conexión BIEN DE AVERGA";
-    }
+<?php
+    $contarsena = "Ally.2203"; 
+    $usuario = "postgres";
+    $host_db = "localhost";
+    $nombre_db = "db_cLapaz";
 
-    $result = pg_query($connection, "SELECT * FROM tbl_alumnos");
-    ?>
-    <table>
-        <tr>
-            <th>nie</th>
-            <th>nombre</th>
-            <th>apellido</th>
-        </tr>
-        <?php
-        if (pg_num_rows($result) > 0) {
-            while ($row = pg_fetch_assoc($result)) {
-                echo "
-                <tr>
-                    <td>" . $row["nie"] . "</td>
-                    <td>" . $row["nombre"] . "</td>
-                    <td>" . $row["apellido"] . "</td>
-                </tr>
-                ";
-            }
-        } else {
-            echo "<tr><td colspan='3'>No hay datos disponibles</td></tr>";
-        }
-        ?>
-    </table>
-</body>
-</html>
+    //conexion
+    try{
+        $miDB = new PDO("pgsql:host=$host_db;dbname=$nombre_db",$usuario, $contarsena);
+        
+    }catch(PDOException $e){
+        echo "<p>Error al conectar a la base de datos: ". $e
+        ->getMessage() . "</p>\n";
+        die();
+    }
+?>
+
